84eb0d72aa56de4ec9ec2a43a5f12b6344fa63cf
Alexander Yambao <32828383+JohnHiro84@users.noreply.github.com>
2020-08-20 21:16:10 -0700

    Create keys.js

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // mongoURI: 'mongodb://AtlasAdmin:Admin6789@cluster0-shard-00-00-yx83e.mongodb.net:27017,cluster0-shard-00-01-yx83e.mongodb.net:27017,cluster0-shard-00-02-yx83e.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+  mongoURI: 'mongodb://AtlasAdmin:Admin6789@cluster0-shard-00-00-yx83e.mongodb.net:27017,cluster0-shard-00-01-yx83e.mongodb.net:27017,cluster0-shard-00-02-yx83e.mongodb.net:27017/merntwitter?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  secretOrKey: "secret123abc789",
+  stripeKey: "sk_test_51H6pQPKx4XF3FVjgHPJi6Kjex0wWejUYoDKBm81VA2bEsZ2ArGb9FL5x09fdSKpUIWDNoepTfu5V7WDjRgoMgo7N001e4saCNW"
+            //Make sure this is your own unique string
+}
